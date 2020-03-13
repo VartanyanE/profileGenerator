e7a561fed6f93e9dc91f5c228357f78086897d94
mikevampm323 <mike.vartanyan@yahoo.com>
2020-03-12 18:03:49 -0700

    added array objects

--- app.js
+++ app.js
@@ -4,125 +4,112 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const util = require("util");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
+let employeeData = [];
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const adminOptions = [
+    {
+        type: "list",
+        message: "Would you like to:",
+        name: "adminOptions",
+        choices: [
+            'Add an employee to the team?',
+            'Create the team HTML page?'
+        ]
+    }
+]
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+const adminQuestions = [
+    {
+        type: "input",
+        message: "Hello manager, what is your name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },
+    {
+        type: "confirm",
+        message: "Are you a manager?",
+        name: "position",
+        choices: [
+            'Yes',
+            'No'
+        ]
+    }
+];
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const questions = [
+    {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the employee's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "What is the employee's title?",
+        name: "title",
+        choices: [
+            'engineer',
+            'intern'
+        ]
+    }
+];
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is your name?",
-            name: "employee_name"
-        },
-        {
-            type: "input",
-            message: "What is your employee ID?",
-            name: "employee_Id"
-        },
-        {
-            type: "input",
-            message: "What is your email?",
-            name: "employee_email"
-        },
-        {
-            type: "list",
-            message: "What is your current position?",
-            name: "employee_position",
-            choices: ["Manager", "Engineer", "Intern"],
-        },
-        {
-            type: "input",
-            message: "What is your office number?",
-            name: "manager_officeNumber",
-            when: (answers) => answers.employee_position === "Manager",
-        },
-        {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "engineer_githubUN",
-            when: (answers) => answers.employee_position === "Engineer",
-        },
-        {
-            type: "input",
-            message: "What school do you attend?",
-            name: "intern_school",
-            when: (answers) => answers.employee_position === "Intern",
-        }
-    ])
-        .then(answers => {
-            if (answers.employee_position === "Manager") {
-                var newManager = new Manager(answers.name, answers.employee_Id, answers.employee_email, answers.manager_officeNumber);
-                rendernewManager(answers);
+const managerQuestion = [
+    {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+    }
 
-            } else if (answers.employee_position === "Engineer") {
-                var newEngineer = new Engineer(answers.name, answers.employee_Id, answers.employee_email, answers.engineer_githubUN);
-                console.log(newEngineer);
-            } else if (answers.employee_position === "Intern") {
-                var newIntern = new Intern(answers.name, answers.employee_Id, answers.employee_email, answers.intern_school);
-            }
-        })
-}
+];
 
-const rendernewManager = (answers) => {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>My Team</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    </head>
-    
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
-                    {{ team }}
-                </div>
-            </div>
-        </div>
-    </body>
-    
-    </html>`
-}
+const engineerQuestion = [
+    {
+        type: "input",
+        message: "What is the employee's GithUb username?",
+        name: "gitusername"
+    }
 
-promptUser();
+];
+
+const internQuestion = [
+    {
+        type: "input",
+        message: "What school did the employee go to?",
+        name: "school"
+    }
+
+];
+
+
